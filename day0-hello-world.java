920cd83f53d21f100dbee7ef1f99f9f904d7c204
jincito <110742524+jincito@users.noreply.github.com>
2024-05-03 12:28:56 -0400

    Complete solution to day0 HackerRank challenge hello world

---
+++ day0-hello-world.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    Scanner scan = new Scanner(System.in);
+
+    String inputString = scan.nextLine();
+
+    scan.close();
+
+    System.out.println("Hello World");
+    System.out.println(inputString);
+}
