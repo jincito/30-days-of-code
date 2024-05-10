7a4273b2a40936c2a6eb3729a92fa5e0170c3302
jincito <110742524+jincito@users.noreply.github.com>
2024-05-10 13:40:26 -0400

    Complete practice

---
+++ src/day8-dictionaries-maps/Day8.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Map<String,Integer> phoneBook = new HashMap<String,Integer>();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            phoneBook.put(name,phone);
+        }
+        
+        while(in.hasNext()){
+            String s = in.next();
+            if (phoneBook.containsKey(s)) {
+                System.out.println(s + "=" + phoneBook.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
+
